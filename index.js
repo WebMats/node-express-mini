19f6ed009598de1f3ff430944ae3328f984f74d0
WebMats <lorlando127@gmail.com>
2019-02-11 15:00:42 -0500

    initial setup done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// implement your API here
+const express = require('express');
+const app = express();
+const router = express.Router();
+app.use(express.json());
+
+
+
+app.use('/api/users', router);
+
+router.post('', (req, res, next) => {
+
+})
+router.get('', (req, res, next) => {
+    console.log('ACTIVE')
+})
+router.get('/:id', (req, res, next) => {
+    
+})
+router.delete('/:id', (req, res, next) => {
+    
+})
+router.put('/:id', (req, res, next) => {
+    
+})
+
+
+
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000...')
+})
